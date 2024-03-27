93ea619bf5d125b5eac84b07a7f6d4acf1127a7c
NehalSurti <nehalsurti9998@gmail.com>
2024-03-27 23:53:36 +0000

    Updating

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import styles from "./links.module.css";
+import styles from "./Links.module.css";
 import NavLink from "./navLink/navLink";
 import Image from "next/image";
 import { handleLogout } from "@/lib/action";
